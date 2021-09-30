e1c73b0fee6da96b64cedd768b314eb00a14a229
Anar <aanar1k14@gmail.com>
2021-09-30 12:56:04 +0200

    Удаление чатов

--- src/Components/ChatsList/ChatsList.jsx
+++ src/Components/ChatsList/ChatsList.jsx
@@ -1,30 +1,49 @@
-import { makeStyles } from "@material-ui/core/styles";
 import { List, ListItem } from "@material-ui/core/";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    maxWidth: 200,
-    position: "fixed",
-    top: "30%",
-    left: "400px",
-  },
-});
+const Users = styled.div`
+  width: 100%;
+  maxwidth: 200;
+  position: fixed;
+  top: 30%;
+  left: 400px;
+`;
 
-const ChatsList = ({ chats }) => {
-  const classes = useStyles();
+const ButtonDelete = styled.button`
+  color: white;
+  outline: none;
+  cursor: pointer;
+  padding: 3px 15px;
+  background: #282c34;
+  border: 1px solid white;
+  transition: 0.4s;
+  margin-right: 10px;
+  &:hover {
+    color: #282c34;
+    border: 1px solid #282c34;
+    background: white;
+  }
+`;
+
+const ChatsList = ({ chats, setChats }) => {
+  const deleteUserDialog = (user) => {
+    setChats((prevstate) => prevstate.filter((item) => item !== user));
+  };
 
   return (
-    <div className={classes.root}>
+    <Users>
       <List>
         {chats.map((el) => (
-          <ListItem button key={el.id}>
+          <ListItem key={el.id}>
+            <ButtonDelete type="button" onClick={() => deleteUserDialog(el)}>
+              X
+            </ButtonDelete>
             <Link to={`/users/${el.id}`}>{el.name}</Link>
           </ListItem>
         ))}
       </List>
-    </div>
+    </Users>
   );
 };
 
--- src/Pages/PageChats/PageChats.jsx
+++ src/Pages/PageChats/PageChats.jsx
@@ -4,18 +4,17 @@ import ChatsList from "../../Components/ChatsList/ChatsList";
 import ChatItem from "../../Components/ChatItem/ChatItem";
 import ChatForm from "../../Components/ChatForm/ChatForm";
 
-const chats = [
-  { id: 1, name: "Egor", messages: [] },
-  { id: 2, name: "Nikita", messages: [] },
-  { id: 3, name: "Anar", messages: [] },
-  { id: 4, name: "Petr", messages: [] },
-];
-
 const Chats = () => {
   const [messageList, setMessageList] = useState([]);
+  const [chats, setChats] = useState([
+    { id: 1, name: "Egor", messages: [] },
+    { id: 2, name: "Nikita", messages: [] },
+    { id: 3, name: "Anar", messages: [] },
+    { id: 4, name: "Petr", messages: [] },
+  ]);
   return (
     <>
-      <ChatsList chats={chats} />
+      <ChatsList chats={chats} setChats={setChats} />
       <Switch>
         <Route exact path="/users/:userId">
           <ChatItem messages={messageList} chats={chats} />
